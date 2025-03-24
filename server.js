28a2147a601d878ebe4e04bc0304d5bdae5c66a7
arjun-rvhs <palakkal_arjun_sarath@students.edu.sg>
2025-03-24 13:49:54 +0800

    Added 4 facilities, modified 1: Added 'Rejuvenate!' to spotlights, added 'Auditorium', 'Grandstand', and 'Fitness Corner' to etc facilities, and changed 'School Track' to 'Field'

--- server.js
+++ server.js
@@ -28,14 +28,18 @@ const locationData = {
     "canteen": new Location("School Canteen", "bg_canteen.png", 0, "https://panoraven.com/en/embed/i0s8Ls8ekZ", "The daily hub of flavor and chatter, served fresh."),
     "scholars-court": new Location("Scholar's Court", "bg_scholar.png", 0, "https://panoraven.com/en/embed/YhRO2FnwMW", "Where minds meet and ideas take root. A space for quiet brilliance."),
     "the-spring": new Location("The Spring", "bg_spring.png", 0, "https://panoraven.com/en/embed/nwE79dyoOX", "The finest mental health support Singapore has to offer."),
+    "rejuvenate": new Location("Rejuvenate!", "bg_rejuvenate.png", 0, "https://panoraven.com/en/embed/rhL2j8pgkg", "A place to pause, unwind, and recharge. Comfort meets necessity."),
 
 
     // NPC Ahh Locations
+    "auditorium": new Location("Auditorium", "bg_audi.png", 0, "https://my.matterport.com/show/?m=ZPw8SBYkJ8d", "The stage for everything that matters."),
     "good-news-cafe": new Location("Good News Café", "bg_gnc.png", 0, "https://panoraven.com/en/embed/Wc82IialIq", "The taste that lingers. It's flavor. It's comfort. It's your new obsession."),
-    "school-track": new Location("School Track", "bg_track.png", 0, "https://panoraven.com/en/embed/tXA0EeT77o", "The path where endurance meets determination."),
+    "field": new Location("Field", "bg_field.png", 0, "https://panoraven.com/en/embed/MMosdULmaN", "Where games are played, dreams are chased, and every blade of grass has a story."),
+    "grandstand": new Location("Grandstand", "bg_grandstand.png", 0, "https://panoraven.com/en/embed/w9GqbCFDvg", "Where pride is raised and history stands tall. Flags fly, spirits rise."),
     "foyer": new Location("Foyer", "bg_foyer.png", 0, "https://panoraven.com/en/embed/3u1CkASOn7", "The gateway to everything."),
     "fce-room": new Location("FCE Room", "bg_fce.png", 2, "https://panoraven.com/en/embed/5ASV5TvQwW", "Where ingredients become masterpieces."),
     "big-e": new Location("Big E", "bg_big_e.png", 1, "https://panoraven.com/en/embed/A7rQMrzlGA", "A hub for problem-solvers."),
+    "fitness-corner": new Location("Fitness Corner", "bg_fitness_corner.png", 1, "https://panoraven.com/en/embed/bM4Yg7riwj", "Where strength is built and limits are tested. Push past the usual."),
     "table-tennis-room": new Location("Table Tennis Room", "bg_tt_room.png", 4, "https://panoraven.com/en/embed/ilsi8v3GxJ", "Where every serve is a strategy."),
     "art-room": new Location("Art Room", "bg_art.png", 1, "https://panoraven.com/en/embed/HTDqWcnLoO", "Where imagination meets canvas."),
     "slda-room": new Location("SLDA Room", "bg_slda.png", 1, "https://panoraven.com/en/embed/9GSOZlhFUO", "The forge where leaders are shaped."),
